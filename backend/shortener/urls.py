b4dd90c38ec1f847ba7f7c357a313bbf17667f6f
Svennson17 <kiryl.shvedau@gmail.com>
2023-01-14 09:07:02 +0100

    feat: API working version

--- backend/shortener/urls.py
+++ backend/shortener/urls.py
@@ -5,9 +5,9 @@ appname = "shortener"
 
 urlpatterns = [
     path("", views.CreateShortUrlView.as_view(), name="home"),
-    # path(
-    #     '<str:shortened_part>',
-    #     views.RedirectUrlView.as_view(),
-    #     name='redirect'
-    # ),
+    path(
+        '<str:shortened_part>',
+        views.RedirectUrlView.as_view(),
+        name='redirect'
+    ),
 ]
