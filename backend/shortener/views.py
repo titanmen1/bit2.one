13cf619b8a7790ccd94b60f49b52a64ec018467f
Svennson17 <kiryl.shvedau@gmail.com>
2023-01-14 09:07:02 +0100

    feat: API working version

--- backend/shortener/views.py
+++ backend/shortener/views.py
@@ -3,23 +3,28 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from shortener.serializers import ShortenerSerializer
+from django.shortcuts import redirect
 
 
 class CreateShortUrlView(APIView):
-    def get(self, request, *args, **kwargs):
-        shorteners = Shortener.objects.filter()
-        serializer = ShortenerSerializer(shorteners, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    # def get(self, request, *args, **kwargs):
+    #     shorteners = Shortener.objects.filter()
+    #     serializer = ShortenerSerializer(shorteners, many=True)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
         data = {
-            # 'short_url': request.data.get('short_url'),
             'full_url': request.data.get('full_url'),
-            # 'created_at': request.data.get('created_at'),
-            # 'updated_at': request.data.get('updated_at')
         }
         serializer = ShortenerSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            result = self.request.build_absolute_uri('/') + serializer.instance.short_url
+            return Response({'result': result}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RedirectUrlView(APIView):
+    def get(self, request, *args, **kwargs):
+        shortener = Shortener.objects.get(short_url=kwargs['shortened_part'])
+        return redirect(shortener.full_url)
